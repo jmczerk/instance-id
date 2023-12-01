e6d46269d906dd179509558630ba249969495108
John Czerkowicz <jczerk@verily.com>
2023-11-30 22:11:14 -0500

    test invoke

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"context"
 	"flag"
+	"io"
 	"log"
+	"net/http"
+	"net/url"
 
+	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
 	"github.com/jmczerk/instance-id/utils"
 )
 
@@ -14,6 +18,32 @@ var (
 	sleepTime  = flag.Int("max-sleep-time", 10, "Max time to sleep between iterations")
 )
 
+func invoke(presigned *v4.PresignedHTTPRequest) string {
+	req := http.Request{
+		Method: presigned.Method,
+		URL: func() *url.URL {
+			parse, err := url.Parse(presigned.URL)
+			if err != nil {
+				panic(err)
+			}
+			return parse
+		}(),
+		Header: presigned.SignedHeader,
+	}
+
+	rsp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatalf("Invoke HTTP request failed: %v", err)
+	}
+
+	body, err := io.ReadAll(rsp.Body)
+	if err != nil {
+		log.Fatalf("Read body failed: %v", err)
+	}
+
+	return string(body)
+}
+
 func main() {
 	ctx := context.Background()
 	auth, err := utils.AuthenticateInstance(ctx)
@@ -22,5 +52,5 @@ func main() {
 		log.Fatalf("AuthenticateInstance failed: %v", err)
 	}
 
-	log.Println(auth)
+	log.Println(invoke(auth))
 }
